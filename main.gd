21146a821656ae3e37cc764288cd3b2e9dafb57d
Andrew Davis <jonandrewdavis@gmail.com>
2025-10-22 22:06:05 -0400

    stationary cam bool

--- main.gd
+++ main.gd
@@ -1,6 +1,10 @@
 extends Node3D
 
+@export var stationary_cam := false
 @onready var window: Window = get_window()
 
 func _ready() -> void:
 	%Floor.set_collision_layer_value(5, true)
+	
+	if stationary_cam: 
+		%StationaryCamera.current = true
