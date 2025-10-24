ee83102e3c7cc543688b1dcbc2eeaa31a2c340f9
Andrew Davis <jonandrewdavis@gmail.com>
2025-10-24 19:53:19 -0400

    remove bloom, collision for val test scene

--- main.gd
+++ main.gd
@@ -4,13 +4,4 @@ extends Node3D
 @onready var window: Window = get_window()
 
 func _ready() -> void:
-	%Floor.set_collision_layer_value(5, true)
-	
-	if stationary_cam: 
-		%StationaryCamera.current = true
-		%StationaryCamera.add_to_group("StationaryCam")
-
-#func _process(_delta: float) -> void:
-	#if stationary_cam:
-		#var player = get_tree().get_first_node_in_group('Players')
-		#%StationaryCamera.look_at(player.position)
+	pass
--- objects/volleyball/volley_ball.gd
+++ objects/volleyball/volley_ball.gd
@@ -11,79 +11,41 @@ func _ready() -> void:
 	
 	set_collision_layer_value(1, false)
 	set_collision_mask_value(1, true)
-	set_collision_mask_value(2, true) # Paddle collision
 
-	shape_cast_floor.set_collision_mask_value(1, false)
-	shape_cast_floor.set_collision_mask_value(5, true)
 	shape_cast_floor.top_level = true
-
 	torus_indicator.top_level = true
+	ray_cast_down.top_level = true
 	
 	await get_tree().create_timer(0.2).timeout
 	var rand_v = randf_range(-4.0, 4.0) * Vector3(1.0, -0.5, 1.0)
 	
 	var BLAST = 250.0
 	apply_central_force(rand_v * BLAST)
-
-	%PickupArea.body_entered.connect(on_pickup)
-	
-	await get_tree().create_timer(0.3).timeout
 	is_showing_line = true
-	
-	#if not is_multiplayer_authority():
-		#set_process(false)
-		#set_physics_process(false)
-
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("debug1"):
-		apply_central_force(Vector3.UP * 500.0)
-
 	move_ray_casts()
 	check_collision()
 
 func move_ray_casts():
-	ray_cast_down.position = position
+	ray_cast_down.position = position + Vector3(0.0, 40.0, 0.0)
 	shape_cast_floor.position = position
 	
-
 func check_collision():
 	if ray_cast_down.is_colliding():
 		torus_indicator.position = ray_cast_down.get_collision_point()
-		#torus_indicator.scale = position.distance_to(ray_cast_down.get_collision_point())
-		#print(ray_cast_down.is_colliding())
-		#print(ray_cast_down.get_collision_point())
-		
-	if shape_cast_floor.is_colliding() == false and is_showing_line:
-		line(global_position, ray_cast_down.get_collision_point())
-		#var collision_obj = shape_cast_floor.get_collider(0)
-		#print('[Debug]: Ball on floor: ', collision_obj)
-		#serve_ball()
 
-func serve_ball():
-	is_serving = true
-	freeze = true
-	var rand_pos = get_random_point_in_square(Vector2(0, 0), Vector2(10.0, 10.0))
-	var serve_height = 10.0	
-	# Note: x, y, z, but we use the 2D square, excercise caution.
-	position = Vector3(rand_pos.x, serve_height, rand_pos.y)
-	await get_tree().create_timer(0.1).timeout
-	is_serving = false
-	freeze = false
+
+	if is_showing_line and ray_cast_down.is_colliding:
+		line(global_position, ray_cast_down.get_collision_point())
+
 
 func get_random_point_in_square(pos: Vector2, size: Vector2) -> Vector2:
 	# Generate a random X coordinate within the square's horizontal bounds
 	var random_x = randf_range(pos.x, pos.x + size.x)
 	# Generate a random Y coordinate within the square's vertical bounds
 	var random_y = randf_range(pos.y, pos.y + size.y)
-
 	return Vector2(random_x, random_y)	
-
-func on_pickup(body):
-	if body.is_in_group('Players'):
-		body.add_item()
-		#if Engine.is_editor_hint() == false:
-			#queue_free()
 
 
 func line(pos1: Vector3, pos2: Vector3, color = Color.AQUA, persist_ms = 1):
