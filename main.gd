f25b91263c74f8c47e025334f2a63eadd918495e
Andrew Davis <jonandrewdavis@gmail.com>
2025-10-26 15:04:14 -0400

    done

--- main.gd
+++ main.gd
@@ -189,7 +189,7 @@ func spawn_ingredient():
 		await get_tree().create_timer(randf_range(0.2, 0.5)).timeout
 		var new_ingredient: Ingredient = ingredient.instantiate()
 		var random_radians = randi_range(0, 360)
-		new_ingredient.position = get_point_on_circumference(Vector2.ZERO, 18.0, random_radians)
+		new_ingredient.position = get_point_on_circumference(Vector2.ZERO, 16.0, random_radians)
 		new_ingredient.initial_angle =  get_point_on_circumference(Vector2.ZERO, 1.0, random_radians - 10)
 		new_ingredient.type = get_ingredient_chance(levels[current_level][current_round])
 		add_child(new_ingredient, true)
